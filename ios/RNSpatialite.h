52536af5cbc984c5609d27e4ce8ebc2e1d3c5f5c
DatNgo <dc2dev@Mac-mini.local>
2017-08-22 15:38:03 +0700

    first commit

---
+++ ios/RNSpatialite.h
@@ -0,0 +1,14 @@
+
+#if __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#else
+#import <React/RCTBridgeModule.h>
+#endif
+
+@interface RNSpatialite : NSObject <RCTBridgeModule>
+- (void)openFromFilename:(NSString *)filename callback:(RCTResponseSenderBlock)callback;
+- (void)closeDatabase:(NSString *)databaseId callback:(RCTResponseSenderBlock)callback;
+- (void)prepareStatement: (NSString *)databaseId sql: (NSString *)sql andParams: (NSArray *)params callback: (RCTResponseSenderBlock)callback;
+- (void)stepStatement:(NSString *)databaseId statementId: (NSString *) statementId callback:(RCTResponseSenderBlock)callback;
+- (void)finalizeStatement:(NSString *)databaseId statementId: (NSString *) statementId callback:(RCTResponseSenderBlock)callback;
+@end
